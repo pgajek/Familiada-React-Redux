f9c5ed426beeeddf2fd3316a8de6716930ccdff2
Patryk Gajek <pgajek94@gmail.com>
2019-04-10 12:59:31 +0200

    Updates

---
+++ precache-manifest.bdeb265617e20c3fca8722b17baf9f72.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d75cdcb184b145ceee9c",
+    "url": "/Familiada-React/static/css/main.1ad015db.chunk.css"
+  },
+  {
+    "revision": "d75cdcb184b145ceee9c",
+    "url": "/Familiada-React/static/js/main.b56e586c.chunk.js"
+  },
+  {
+    "revision": "52fcee7386cdda67fd55",
+    "url": "/Familiada-React/static/js/runtime~main.8c4d61e9.js"
+  },
+  {
+    "revision": "3459532d875e0fb29e8b",
+    "url": "/Familiada-React/static/js/2.4e8b7d57.chunk.js"
+  },
+  {
+    "revision": "11bd628686f671eeddfcee314c228825",
+    "url": "/Familiada-React/static/media/fail.11bd6286.mp3"
+  },
+  {
+    "revision": "e57bec627d279cc9798c4f1e61d6680e",
+    "url": "/Familiada-React/static/media/intro.e57bec62.mp3"
+  },
+  {
+    "revision": "6e028a88355320731cebad887ad99aa8",
+    "url": "/Familiada-React/static/media/good.6e028a88.mp3"
+  },
+  {
+    "revision": "bed6fdce1a9a6d63d6f5f6c2bad6cbff",
+    "url": "/Familiada-React/static/media/familiada.bed6fdce.woff"
+  },
+  {
+    "revision": "2e6dc538968f0bf86b4b3cfdac7fd937",
+    "url": "/Familiada-React/static/media/familiada.2e6dc538.eot"
+  },
+  {
+    "revision": "d0c2996cade82938e5d0c5131f132d0e",
+    "url": "/Familiada-React/static/media/familiada.d0c2996c.ttf"
+  },
+  {
+    "revision": "33b1f61cfdb35373ff9fa76538a784da",
+    "url": "/Familiada-React/static/media/familiada.33b1f61c.svg"
+  },
+  {
+    "revision": "d2d4054cef16a10f01521a645bbc6a43",
+    "url": "/Familiada-React/index.html"
+  }
+];
